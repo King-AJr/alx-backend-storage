5d474db6c6e5acda55616f429bc344711601693e
King-AJr <talk2king.aj@gmail.com>
2023-09-01 10:34:58 +0100

    correct annotation for data arg of store method and comment code properly

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ creating a simple redis cache
 """
 import redis
 import uuid
-from typing import Any
+from typing import Union
 
 
 class Cache():
@@ -14,18 +14,34 @@ class Cache():
     """
     def __init__(self):
         """
-        instantiate a redis client as
-        a redis variable and flush the
-        instance using flushdb.
+        Initialize a Redis client and flush the Redis database.
+
+        This constructor method creates an instance of a Redis
+        client using the `redis.Redis()` constructor and also
+        defines a `flush` attribute, which is a reference to the
+        `flushdb` method of the Redis client instance. This allows
+        you to easily flush the Redis database when needed.
+
+        Parameters:
+            None
+
+        Returns:
+            None
         """
         self._redis = redis.Redis()
         self.flush = self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        recieves a data and stores
-        it in the redis cache
+        Receives a string data and stores it in the Redis cache.
+
+        Args:
+            self: An instance of the class containing this method.
+            data (str): The string data to be stored in the Redis cache.
+
+        Returns:
+            str: The unique key used to store the data in the Redis cache.
         """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        key = str(uuid.uuid4())  # Generate a unique key
+        self._redis.set(key, data)  # Store the data in the Redis cache
+        return key  # Return the unique key used for storage
