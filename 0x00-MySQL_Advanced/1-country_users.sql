5013f4faef6020c1ff5b9b9a83938936660086ab
King-AJr <talk2king.aj@gmail.com>
2023-09-10 19:55:32 +0000

    ensure country column value is in a specific set and is not null

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
-    country VARCHAR(255) CHECK (country IS NULL OR country IN ('US', 'CO', 'TN'))
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
